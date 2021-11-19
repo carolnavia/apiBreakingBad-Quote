4b5415785b2f64c0e03fc6706f2ad3627e486bba
carolnavia <carollnavia1913@gmail.com>
2021-11-19 16:35:34 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Quote } from "./components/Quote";
+import { Spinner } from "./components/Spinner";
+
+const initialQuote = {
+  text: "Quote",
+  author: "Author",
+};
 
 function App() {
+  const [quote, setQuote] = useState(initialQuote);
+  const [loading, setLoading] = useState(false);
+
+  const upDateQuote = async () => {
+    setLoading(true);
+    const url = "https://www.breakingbadapi.com/api/quote/random";
+    const res = await fetch(url);
+    const [newQuote] = await res.json();
+    // setQuote({
+    //   text: newQuote.quote,
+    //   author: newQuote.author,
+    // });
+
+    const { quote: text, author } = newQuote;
+
+    setQuote({
+      text,
+      author,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    upDateQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo"
+      />
+      <button onClick={() => upDateQuote()}> Get Another</button>
+      {loading ? <Spinner /> : <Quote quote={quote} />}
     </div>
   );
 }
---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Spinner.css";
+
+export const Spinner = () => {
+  return (
+    <div className="lds-default">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
